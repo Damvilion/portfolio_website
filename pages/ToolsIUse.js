65188b96c9dabb3f7ef1b102a2b31cb3ef302bd8
Noel <noelohaeri@gmail.com>
2023-06-25 21:37:57 -0400

    Added snap-functionality

--- pages/ToolsIUse.js
+++ pages/ToolsIUse.js
@@ -34,71 +34,77 @@ const ToolsIUse = () => {
       </div>
       <div className="flex items-center justify-center">
         <MdChevronLeft
-          size={40}
-          // onClick={slideLeft}
           onClick={() => scroll(-300)}
-          className="opacity-50 cursor-pointer hover:opacity-100 mx-2"
+          className="opacity-50 cursor-pointer hover:opacity-100"
         />
 
         <div
           ref={slider}
           id="slider"
-          className="flex flex-wrap gap-2 md:flex-row scrollbar-hide lg:w-full lg:h-full md:overflow-x-scroll md:scroll-smooth md:whitespace-nowrap lg:p-1 md:flex-nowrap"
+          className="flex flex-wrap flex-col items-center gap-2 w-full snap-x md:flex-row scrollbar-hide lg:w-full lg:h-full md:overflow-x-auto md:scroll-smooth md:whitespace-nowrap lg:p-1 md:flex-nowrap"
         >
-          <div className="snap-center text-center shadow-lg rounded-xl my-10 hover:shadow-2xl duration-100 hover:translate-y-1">
-            <Image
-              src={react_2}
-              width={100}
-              height={100}
-              alt="react_logo"
-              className="mx-auto"
-            />
+          <div className="snap-center">
+            <div className="text-center shadow-lg rounded-xl my-10 hover:shadow-2xl duration-100 hover:translate-y-1">
+              <Image
+                src={react_2}
+                width={100}
+                height={100}
+                alt="react_logo"
+                className="mx-auto"
+              />
 
-            <h3 className="text-lg font-medium pt-8 pb-2">React JS</h3>
-            <div className="py-2 mx-2">
-              <p>React is a JavaScript library for building user interfaces.</p>
-              <p>
-                It uses reusable components and a virtual DOM for efficient
-                rendering,
-              </p>
-              <p>making it popular for creating dynamic web applications</p>
+              <h3 className="text-lg font-medium pt-8 pb-2">React JS</h3>
+              <div className="py-2 mx-2">
+                <p>
+                  React is a JavaScript library for building user interfaces.
+                </p>
+                <p>
+                  It uses reusable components and a virtual DOM for efficient
+                  rendering,
+                </p>
+                <p>making it popular for creating dynamic web applications</p>
+              </div>
+              <h4 className="py-4 text-teal-600">Learn More</h4>
             </div>
-            <h4 className="py-4 text-teal-600">Learn More</h4>
           </div>
 
-          <div className=" text-center shadow-lg rounded-xl my-10 hover:shadow-2xl duration-100 hover:translate-y-1">
-            <Image
-              src={tailwind_logo}
-              width={100}
-              height={100}
-              alt="react_logo"
-              className="mx-auto"
-            />
+          <div className="snap-center">
+            <div className=" text-center shadow-lg rounded-xl my-10 hover:shadow-2xl duration-100 hover:translate-y-1">
+              <Image
+                src={tailwind_logo}
+                width={100}
+                height={100}
+                alt="react_logo"
+                className="mx-auto"
+              />
 
-            <h3 className="text-lg font-medium pt-8 pb-2">Tailwind CSS</h3>
-            <div className="py-2 mx-2">
-              <p className="py-2 mx-2">
-                Tailwind CSS enables me to create stunning, responsive designs
-                effortlessly.
-              </p>
-              <p>By leveraging its extensive collection of utility classes,</p>
-              <p>
-                I can quickly style and structure websites, saving valuable
-                development time.
-              </p>
+              <h3 className="text-lg font-medium pt-8 pb-2">Tailwind CSS</h3>
+              <div className="py-2 mx-2">
+                <p className="py-2 mx-2">
+                  Tailwind CSS enables me to create stunning, responsive designs
+                  effortlessly.
+                </p>
+                <p>
+                  By leveraging its extensive collection of utility classes,
+                </p>
+                <p>
+                  I can quickly style and structure websites, saving valuable
+                  development time.
+                </p>
+              </div>
+
+              <h4 className="py-4 text-teal-600">Learn More</h4>
             </div>
-
-            <h4 className="py-4 text-teal-600">Learn More</h4>
           </div>
-
-          <Tools name={nodeJs_logo} />
+          <div className="snap-center">
+            <Tools name={nodeJs_logo} />
+          </div>
         </div>
 
         <MdChevronRight
           size={40}
-          // onClick={slideRight}
           onClick={() => scroll(300)}
-          className="opacity-50 cursor-pointer hover:opacity-100 mx-2"
+          className="opacity-50 cursor-pointer hover:opacity-100"
         />
       </div>
     </div>
